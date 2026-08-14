f42d20c18cea4e53daf51b473c76caa668cc7645
mgyoo <mgyoo@autocrypt.io>
2026-08-14 16:17:46 +0900

    기능: Qt 클라이언트 애플리케이션 조립

--- apps/qt-client/src/main.cpp
+++ apps/qt-client/src/main.cpp
@@ -1,6 +1,17 @@
 #include <QApplication>
 
+#include "application/ClientController.h"
+#include "network/QtSessionClient.h"
+#include "ui/MainWindow.h"
+
 int main(int argc, char* argv[]) {
   QApplication application(argc, argv);
-  return 0;
+
+  rss::qt_client::MainWindow window;
+  auto* transport = new rss::qt_client::QtSessionClient(&window);
+  auto* controller = new rss::qt_client::ClientController(*transport, &window);
+  window.bind(*controller);
+  window.show();
+
+  return application.exec();
 }
